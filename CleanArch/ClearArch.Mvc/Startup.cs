3ff697c28f2dc768c9d3cfa873f607ded59d5244
Leonel Estrella <leonel.estrella@jbknowledge.com>
2022-09-07 13:11:12 -0300

    Configure Presentation for Dependency Container

--- CleanArch/ClearArch.Mvc/Startup.cs
+++ CleanArch/ClearArch.Mvc/Startup.cs
@@ -1,4 +1,5 @@
 using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.IoC;
 using ClearArch.Mvc.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -41,6 +42,8 @@ namespace ClearArch.Mvc
                 {
                     options.UseSqlServer(Configuration.GetConnectionString("UniversityDBConnection"));
                 });
+
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,6 +75,10 @@ namespace ClearArch.Mvc
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+        private static void RegisterServices(IServiceCollection services) 
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
