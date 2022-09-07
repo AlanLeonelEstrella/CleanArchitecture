c4fcae4dc2a081142e1bb0769f382fe38476f593
Leonel Estrella <leonel.estrella@jbknowledge.com>
2022-09-07 12:45:23 -0300

    Commit Implementation of Service Repository GetCourses Methods

---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Moldels;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDBContext _ctx;
+        public CourseRepository(UniversityDBContext context)
+        {
+            _ctx = context;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
